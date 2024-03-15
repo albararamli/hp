4bef301b9b15ce1490956945ba99d3cd4a3cb406
Albara Ramli <arramli@ucdavis.edu>
2024-03-15 15:44:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Importing necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # Import the hp3_read function from the hp3 module
 from hp3 import *
 
@@ -13,32 +17,36 @@ data = hp3_read(url)
 left_data = data[data['side'] == "L"]
 right_data = data[data['side'] == "R"]
 
-# Draw the acceleration signal, PPG, and ECG of the left and right sides
+# Draw the acceleration signal of the left and right sides
 left_data.plot(x="index", y=["acc_x", "acc_y", "acc_z"], figsize=(22, 3))
 right_data.plot(x="index", y=["acc_x", "acc_y", "acc_z"], figsize=(22, 3))
 
+# Draw the PPG of the left and right sides
 left_data.plot(x="index", y=["ppg"], figsize=(22, 3))
 right_data.plot(x="index", y=["ppg"], figsize=(22, 3))
 
+# Draw the ECG of the left and right sides
 left_data.plot(x="index", y=["ecg"], figsize=(22, 3))
 right_data.plot(x="index", y=["ecg"], figsize=(22, 3))
 
 # Acceleration Signal on the Left side
+
 # Remove the drift in the acceleration signal of the left side
 left_data_no_drift = hp3_remove_drift(left_data, base=50)
 
 # Display the data after removing the drift
-print(left_data_no_drift)
+display(left_data_no_drift)
 
 # Draw the signal before and after removing the drift for the left side
 draw_drift(left_data_no_drift)
 
 # Acceleration Signal on the Right side
+
 # Remove the drift in the acceleration signal of the right side
 right_data_no_drift = hp3_remove_drift(right_data, base=50)
 
 # Display the data after removing the drift
-print(right_data_no_drift)
+display(right_data_no_drift)
 
 # Draw the signal before and after removing the drift for the right side
 draw_drift(right_data_no_drift)
